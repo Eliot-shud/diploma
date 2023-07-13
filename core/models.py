a79af282cf49c4be92bbcde7eac3fe21cb2f42bd
elyar <elyarkamol1630@gmail.com>
2023-07-13 12:10:31 +0700

    update file initial_001

--- core/models.py
+++ core/models.py
@@ -3,10 +3,4 @@ from django.db import models
 
 
 class User(AbstractUser):
-    REQUIRED_FIELDS = [
-        "first_name",
-        "last_name",
-        "email",
-        "password",
-        "password_repeat"
-    ]
+    REQUIRED_FIELDS = []
